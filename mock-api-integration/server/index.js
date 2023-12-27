122d3cabf8944b8ecefc6b33ba895231c4e8d897
gubali <masif_apps@live.com>
2023-12-27 11:21:29 +0530

    fake api implementation

---
+++ mock-api-integration/server/index.js
@@ -0,0 +1,6 @@
+const getUsers = require('../server/json/getUsers.json');
+
+module.exports = {
+  getUsers: getUsers
+};
+
--- mock-api-integration/src/app/app.component.ts
+++ mock-api-integration/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mock-api-integration';
+  constructor(private http:HttpClient){}
+  ngOnInit(): void {
+    debugger;
+    this.http.get('http://localhost:3000/api/users').subscribe((data)=>{
+      console.log(data);
+    })
+  }
 }
